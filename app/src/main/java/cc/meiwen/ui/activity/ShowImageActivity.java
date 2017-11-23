fa6839e5d4bb1db4a9b9a53cd27cd3d69f48516b
kendada <859984335@qq.com>
2017-11-23 17:40:36 +0800

    展示大图，长按保存图片；

--- app/src/main/java/cc/meiwen/ui/activity/ShowImageActivity.java
+++ app/src/main/java/cc/meiwen/ui/activity/ShowImageActivity.java
@@ -1,14 +1,26 @@
 package cc.meiwen.ui.activity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+
+import java.io.IOException;
+import java.util.Date;
 
 import cc.meiwen.R;
-import cc.meiwen.util.MnAppUtil;
+import cc.meiwen.util.FileUtils;
+import cc.meiwen.util.MnDateUtil;
 
 /**
  * User: 山野书生(1203596603@qq.com)
@@ -25,11 +37,14 @@ public class ShowImageActivity extends BaseActivity {
         context.startActivity(intent);
     }
 
+    private String TAG = ShowImageActivity.class.getSimpleName();
+
     private ImageView image_view;
+    private Bitmap mResource;
 
     private String imgUrl;
 
-    private int screenW = 720; //默认分辨率
+    private FileUtils fileUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +52,19 @@ public class ShowImageActivity extends BaseActivity {
         setContentView(R.layout.activity_show_image_layout);
         getIntentData();
 
-        screenW = MnAppUtil.getPhoneW(getContext());
+        fileUtils = new FileUtils(getContext());
 
         image_view = (ImageView)findViewById(R.id.image_view);
 
         setImageToBitmap();
+
+        image_view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                saveImageToSDCard();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -55,7 +78,49 @@ public class ShowImageActivity extends BaseActivity {
     }
 
     private void setImageToBitmap(){
-        Glide.with(getContext()).load(imgUrl).asBitmap().into(image_view);
+        Glide.with(getContext()).load(imgUrl).asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                mResource = resource;
+                if(resource != null){
+                    image_view.setImageBitmap(resource);
+                    Log.d(TAG,"w = " + resource.getWidth() + ", h = " + resource.getHeight());
+                }
+            }
+        });
+    }
+
+    /**
+     * 保存图片到SD卡
+     * */
+    public void saveImageToSDCard(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("操作提示");
+        builder.setMessage("你确定下载这张图片吗？\r\n" +
+                "保存路径：SD卡下Android/data/cc.meiwen/image目录下");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                saveBitmap(mResource);
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
+    }
+
+    public void saveBitmap(Bitmap bitmap){
+        try {
+            fileUtils.saveBitmap(MnDateUtil.stringByFormat(new Date(),
+                    MnDateUtil.dateFormatFileName)+".jpg", bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(getContext(), "图片已保存在SD卡下Android/data/cc.meiwen/image目录下", Toast.LENGTH_SHORT).show();
     }
 
 }
